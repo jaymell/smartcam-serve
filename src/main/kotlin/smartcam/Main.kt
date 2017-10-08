c37bb209ef0c9966a3ac0ef1927d67bcd28847fd
James Lucas <jamesmlucas@gmail.com>
2017-10-08 01:58:40 -0500

    posting to dynamo, albeit stupidly

--- src/main/kotlin/smartcam/Main.kt
+++ src/main/kotlin/smartcam/Main.kt
@@ -6,26 +6,56 @@ import org.jetbrains.ktor.features.DefaultHeaders
 import org.jetbrains.ktor.response.*
 import org.jetbrains.ktor.gson.*
 import org.jetbrains.ktor.request.receive
-import org.jetbrains.ktor.request.receiveParameters
+//import org.jetbrains.ktor.request.receiveParameters
 import org.jetbrains.ktor.routing.Routing
 import org.jetbrains.ktor.routing.get
 import org.jetbrains.ktor.routing.post
-import org.jetbrains.ktor.util.cast
-
+import software.amazon.awssdk.regions.Region
+//import org.jetbrains.ktor.util.cast
+import software.amazon.awssdk.services.dynamodb.*
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest
+import software.amazon.awssdk.services.dynamodb.model.PutItemResponse
+import java.util.concurrent.CompletableFuture
 
 fun Application.main() {
+    val table = "smartcam-videos"
+    val region = Region.US_WEST_2
+
     install(DefaultHeaders)
     install(GsonSupport) {
-      setPrettyPrinting()
+        setPrettyPrinting()
     }
     install(Routing) {
         get("/") {
             call.respondText("smartcam")
         }
+        get("/videos") {
+            call.respondText("GET /videos")
+            // if no params passed, return for list 15 minutes?
+            // take start and end as unix ms timestamp for range
+            // to get videos from
+            // query dynamodb given that
+            // return results
+        }
         post("/videos") {
-            val stuff = call.receive<Video>()
-            println(stuff)
-            call.respond(stuff)
+            // receive item, validate
+            // it deserializes properly
+            // insert into dynamo
+            // if error, return 500
+            // else return 200
+            val video = call.receive<Video>()
+            val cli: DynamoDBAsyncClient = DynamoDBAsyncClient.builder()
+                .region(region)
+                .build()
+            val videoItem = video.toDynamoRecord()
+            val videoPutRequest: PutItemRequest = PutItemRequest.builder()
+                .tableName(table)
+                .item(videoItem)
+                .build()
+            val resp: CompletableFuture<PutItemResponse> = cli.putItem(videoPutRequest)
+            resp.get()
+            call.respondText("done")
         }
     }
 }
--- src/main/kotlin/smartcam/Video.kt
+++ src/main/kotlin/smartcam/Video.kt
@@ -1,11 +1,27 @@
 package smartcam
 
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue
+
 data class Video(
-  val start: Float,
-  val end: Float,
-  val width: Int,
-  val height: Int,
-  val bucket: String,
-  val key: String,
-  val region: String
+    val camera_id: String,
+    val start: Float,
+    val end: Float,
+    val width: Int,
+    val height: Int,
+    val bucket: String,
+    val key: String,
+    val region: String
 )
+
+fun Video.toDynamoRecord(): HashMap<String, AttributeValue> {
+    return hashMapOf(
+       "camera_id" to AttributeValue.builder().s("1").build(),
+        "time" to AttributeValue.builder().n(start.toString()).build(),
+        "end" to AttributeValue.builder().n(end.toString()).build(),
+        "width" to AttributeValue.builder().n(width.toString()).build(),
+        "height" to AttributeValue.builder().n(height.toString()).build(),
+        "bucket" to AttributeValue.builder().s(bucket).build(),
+        "key" to AttributeValue.builder().s(key).build(),
+        "region" to AttributeValue.builder().s(region).build()
+    )
+}
