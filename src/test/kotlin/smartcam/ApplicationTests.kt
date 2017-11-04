ba054c5c21f079f661c950db15de91c4c6356e4d
James Lucas <jamesmlucas@gmail.com>
2017-11-04 00:42:04 -0500

    working test

---
+++ src/test/kotlin/smartcam/ApplicationTests.kt
@@ -0,0 +1,23 @@
+package smartcam
+
+import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.http.*
+import org.jetbrains.ktor.testing.*
+//import org.jetbrains.ktor.tests.*
+
+import kotlin.test.*
+//import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.ShouldSpec
+
+class GetSlash : ShouldSpec() {
+    init {
+        should("return something worthless") {
+           withTestApplication(Application::main) {
+               with(handleRequest(HttpMethod.Get, "/")) {
+                   assertEquals(HttpStatusCode.OK, response.status())
+                   assertEquals("smartcam", response.content)
+               }
+           }
+        }
+    }
+}
